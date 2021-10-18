8eab5641d32792ca274e70a8a34ae09f9edf9425
LucasDesktop <lucas.derksen@fourtress.nl>
2021-10-18 13:39:42 +0200

    changed alot

--- Todos_SpringBoot/src/test/java/com/samarmohan/todosspringboot/TodosSpringBootApplicationTests.java
+++ Todos_SpringBoot/src/test/java/com/samarmohan/todosspringboot/TodosSpringBootApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.samarmohan.todosspringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TodosSpringBootApplicationTests {
 
 	@Test
 	void contextLoads() {
